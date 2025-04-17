7d35907540daebf9cf00bea42be6114419e32d88
hamza5588 <y7hamzakhanswati@gmail.com>
2025-04-17 12:42:01 -0700

    file are added

--- quizapp/quizapp/settings.py
+++ quizapp/quizapp/settings.py
@@ -30,7 +30,13 @@ SECRET_KEY = 'django-insecure-u*-el4bk9$g^86p=j(_ki$(w*%4l_4p)mza-#r^6zjlqo@y##d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mcqsbank-3.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'mcqsbank-1.onrender.com',
+    'mcqsbank-2.onrender.com',
+    'mcqsbank-3.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
