d6efc006ab52c16b9f8b68bac4ab359d7301726e
aimankahim <aimankahim1@gmail.com>
2025-04-20 14:16:40 +0500

    Update learning.ts

--- quizchat-magic/src/services/learning.ts
+++ quizchat-magic/src/services/learning.ts
@@ -46,7 +46,7 @@ interface Note {
 }
 
 class LearningService {
-  private baseURL = 'http://localhost:8000/api';
+  private baseURL = 'https://django-based-mcq-app.onrender.com/api';
 
   private async makeRequest<T>(endpoint: string, data: any): Promise<T> {
     try {
