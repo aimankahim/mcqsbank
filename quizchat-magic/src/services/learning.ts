7b4c6a19e9b981e7f2a7ace559cb95e3a5006a8b
hamza5588 <y7hamzakhanswati@gmail.com>
2025-04-24 13:05:19 -0700

    file are added

--- quizchat-magic/src/services/learning.ts
+++ quizchat-magic/src/services/learning.ts
@@ -46,7 +46,7 @@ interface Note {
 }
 
 class LearningService {
-  private baseURL = 'https://django-based-mcq-app.onrender.com/api';
+  private baseURL = 'https://django-based-mcq-app.onrender.com';
 
   private async makeRequest<T>(endpoint: string, data: any): Promise<T> {
     try {
